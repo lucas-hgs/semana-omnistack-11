c0cd30905ac18c6f6b244c1f4ba1919ac3ab192b
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-03-27 18:47:55 -0300

    Finished the Backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    "class-methods-use-this": "error",
+    "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,21 @@
 import { Router } from 'express';
 
+import OngController from './app/controllers/OngController';
+import IncidentController from './app/controllers/IncidentController';
+import ProfileController from './app/controllers/ProfileController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile/', ProfileController.index);
 
 export default routes;
